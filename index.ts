bb7774b70fbe991245e63a60450ff95275cde668
Yamid <yamidnozu@gmail.com>
2020-01-12 12:33:31 -0500

    EYNZ Comentarios

--- index.ts
+++ index.ts
@@ -3,13 +3,20 @@ import router from "./routes/router";
 import bodyParser from 'body-parser'
 import cors from 'cors';
 
-
+// Instancia del servidor
 const server = new Server();
+
+// Configuración del body parser
 server.app.use(bodyParser.urlencoded({ extended: true }));
 // server.app.use(bodyParser.json);
-server.app.use('/', router);
+
+// Configuración de cors
 server.app.use(cors({
     origin: true,
     credentials: true
 }))
+// Configuración de rutas
+server.app.use('/', router);
+
+// Iniciación del servidor
 server.start(() => console.log(`El servidor esta corriendo sobre el puerto: ${server.port}`));
